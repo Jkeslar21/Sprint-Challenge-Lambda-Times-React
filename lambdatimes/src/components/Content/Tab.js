51f7c3e443c5b16af8636367d1a6d92610a06639
jkeslar21 <diamondroad91@gmail.com>
2019-02-22 10:52:18 -0700

    cards + proptypes

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,28 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
-      const activeTab = props.tab === props.selectedTab ? 'tab active-tab' : 'tab';
+  const activeTab = props.tab === props.selectedTab ? 'tab active-tab' : 'tab';
   return (
-    <div
-      className={activeTab}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-         props.selectTabHandler(props.tab)
-      }}
-    >
+    <div className={activeTab} onClick={() => {props.selectTabHandler(props.tab)}}>
       {props.tab.toUpperCase()}
     </div>
   );
 };
 
-// Make sure you include PropTypes on your props.
 //PropTypes
-// Tab.propTypes = {
-
-// }
+Tab.propTypes = {
+  selectedTab: PropTypes.string,
+  tab: PropTypes.string,
+  selectTabHandler: PropTypes.func
+}
 
 export default Tab;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,19 +7,17 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
             {props.tabs.map(tab => <Tab selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} tab={tab} />)}
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
 //PropTypes
-// Tabs.propTypes = {
-//   tabs: PropTypes.string
-
-// }
+Tabs.propTypes = {
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string,
+  tabs: PropTypes.arrayOf(PropTypes.string)
+}
 
 export default Tabs;
