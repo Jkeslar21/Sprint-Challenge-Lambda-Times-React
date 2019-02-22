eeb3fd47879a1158c10ca2c9fe1f954c6b406b46
jkeslar21 <diamondroad91@gmail.com>
2019-02-22 15:17:51 -0700

    touch up

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Tab from './Tab';
 import PropTypes from 'prop-types';
 
-const Tabs = props => {
+const Tabs = ({selectTabHandler, selectedTab, tabs}) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-            {props.tabs.map(tab => <Tab selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} tab={tab} />)}
+            {tabs.map(tab => <Tab selectTabHandler={selectTabHandler} selectedTab={selectedTab} tab={tab} />)}
       </div>
     </div>
   );
